e28097d757cb6aaee20809b3ec6311fc815f012e
Faiz Ismail <faizismail1706@gmail.com>
2023-12-08 16:50:12 +0700

    commit pertama

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Praktikum_Minggu_13',
+    version='0.1.0',
+    author='faizismail06',
+    author_email='faizismal1706@gmail.com',
+    description='berisi tentang tebak angka dan konfersi berat badan',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language == Python == 3',
+        'License == OSI Approved == MIT License',
+        'Operating System == OS Independent',
+    ],
+    
+    python_requires='>=3.6',
+)
